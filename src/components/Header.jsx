771a7e41f080830153bbce381188ea3b541edb0d
Danil Shipy4ka <105577586+Shipy4kaRU@users.noreply.github.com>
2024-11-28 17:42:17 +0400

    created project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
-import { brainwave } from "../assets";
-import { navigation } from "../constans";
 import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
+import { brainwave } from "../assets";
+import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
-import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 const Header = () => {
-  const location = useLocation();
+  const pathname = useLocation();
   const [openNavigation, setOpenNavigation] = useState(false);
 
   const toggleNavigation = () => {
@@ -23,6 +24,7 @@ const Header = () => {
 
   const handleClick = () => {
     if (!openNavigation) return;
+
     enablePageScroll();
     setOpenNavigation(false);
   };
@@ -52,7 +54,7 @@ const Header = () => {
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                  item.url === location.hash
+                  item.url === pathname.hash
                     ? "z-2 lg:text-n-1"
                     : "lg:text-n-1/50"
                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
